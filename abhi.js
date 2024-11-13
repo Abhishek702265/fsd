6cad6ac58f178321cb3e56fb750db7119f5cb6fb
Abhishek702265 <madiwalabhishek981@gmail.com>
2024-11-13 16:32:37 +0530

    Add files via upload

---
+++ abhi.js
@@ -0,0 +1,15 @@
+let a=19,b=20,c=30
+function greater (a,b,c){
+    console.log( 'num is greater')
+}
+if(a>=b && a>=c){
+    return a
+}
+else if(b>=a && b>=c)
+    {
+        return b
+    }
+    else
+    {
+        return c
+    }
